47ec6c82888d862a0a68253af152553d03708303
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-16 09:52:07 +0530

    Created auth enpoints and logic

--- Api/LibrarySystemMinimalApi.Api/Endpoints/AuthEndpoints.cs
+++ Api/LibrarySystemMinimalApi.Api/Endpoints/AuthEndpoints.cs
@@ -1,6 +1,25 @@
-﻿namespace LibrarySystemMinimalApi.Api.Endpoints
+﻿using LibrarySystemMinimalApi.Application.DTOs;
+using LibrarySystemMinimalApi.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystemMinimalApi.Api.Endpoints
 {
-    public class AuthEndpoints
+    public static class AuthEndpoints
     {
+        public static void MapAuthEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("/api/auth").WithTags("Authentication");
+
+            group.MapPost("/login", async (
+                [FromBody] LoginDto loginDto,
+                IAuthenticationService authService) =>
+            {
+               
+                var member = authService.Login(loginDto);
+                return member == null
+                    ? Results.NotFound("Member not found.")
+                    : Results.Ok(member);
+            });
+        }
     }
 }
